e252f9309f8fdffad55ee09c6fa88dd53f32f63c
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-12-26 21:54:26 -0500

    default.nix: add clarifying comment

--- default.nix
+++ default.nix
@@ -2,6 +2,12 @@
 let
   root = {
     name = "root";
+    # This is a helper function that means we don't have to write out the files to be called
+    # However, for posterity, it would expand here to:
+    # modules = {
+    #   nixpkgs = import ./nixpkgs.nix { inherit adios; };
+    #   less = import ./less/default.nix { inherit adios; };
+    # };
     modules = adios.lib.importModules ./modules;
   };
 
