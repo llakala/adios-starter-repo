7fb8257a6b5a8124baa088f879811fb09194e9cc
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-12-26 22:10:49 -0500

    modules/less: add comment explaining input paths

--- modules/less.nix
+++ modules/less.nix
@@ -5,6 +5,36 @@ in {
   name = "less";
 
   inputs = {
+    # This path syntax is weird. It is _not_ a path on disk. Instead, it's a
+    # path "from the root module", following the `modules` tree.
+    #
+    # Remember how the root module (defined in the default.nix) had a modules
+    # attribute that resolved to this?
+    #
+    # modules = {
+    #   nixpkgs = import ./nixpkgs.nix { inherit adios; };
+    #   less = import ./less.nix { inherit adios; };
+    # };
+    #
+    # Well, this "path" uses the attribute names seen here. If we had instead
+    # changed the modules attributes to look like this:
+    #
+    # modules = {
+    #   OTHER_NAME = import ./nixpkgs.nix { inherit adios; };
+    #   less = import ./less.nix { inherit adios; };
+    # };
+    #
+    # Then to use the nixpkgs module as an input, we'd have to do `/other_name`
+
+    # This also works in a nested context. Let's imagine a world where the
+    # nixpkgs module also had its own `modules` attrset, that looked like this:
+    #
+    # # nixpkgs.nix
+    # modules = {
+    #   example = import ./example.nix { inherit adios; };
+    # };
+    #
+    # Then to access the example module as an input, we'd do `/nixpkgs/example`
     nixpkgs.path = "/nixpkgs";
   };
 
